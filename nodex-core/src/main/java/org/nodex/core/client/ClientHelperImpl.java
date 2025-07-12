2de383bf87aa84e1546d7367d62d768dd8058772
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-12 23:39:42 +0000

    feat: Implement ClientHelper with message creation, parsing, and storage methods

---
+++ nodex-core/src/main/java/org/nodex/core/client/ClientHelperImpl.java
@@ -0,0 +1,122 @@
+package org.nodex.core.client;
+
+import org.nodex.api.client.ClientHelper;
+import org.nodex.api.data.BdfDictionary;
+import org.nodex.api.data.BdfList;
+import org.nodex.api.sync.ClientId;
+import org.nodex.api.sync.Group;
+import org.nodex.api.sync.GroupId;
+import org.nodex.api.sync.Message;
+import org.nodex.api.sync.MessageId;
+import org.nodex.api.sync.MessageFactory;
+import org.nodex.api.crypto.CryptoComponent;
+import org.nodex.api.db.DatabaseComponent;
+import org.nodex.api.db.DbException;
+import org.nodex.api.db.Transaction;
+import org.nodex.api.nullsafety.NotNullByDefault;
+import org.nodex.api.data.MetadataEncoder;
+import org.nodex.api.data.MetadataParser;
+
+import javax.annotation.concurrent.Immutable;
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Implementation of ClientHelper that provides utility methods for clients.
+ */
+@Immutable
+@NotNullByDefault
+public class ClientHelperImpl implements ClientHelper {
+
+    private static final Logger LOG = Logger.getLogger(ClientHelperImpl.class.getName());
+
+    private final DatabaseComponent db;
+    private final MessageFactory messageFactory;
+    private final MetadataEncoder metadataEncoder;
+    private final MetadataParser metadataParser;
+    private final CryptoComponent crypto;
+
+    @Inject
+    public ClientHelperImpl(DatabaseComponent db, MessageFactory messageFactory,
+                           MetadataEncoder metadataEncoder, MetadataParser metadataParser,
+                           CryptoComponent crypto) {
+        this.db = db;
+        this.messageFactory = messageFactory;
+        this.metadataEncoder = metadataEncoder;
+        this.metadataParser = metadataParser;
+        this.crypto = crypto;
+    }
+
+    @Override
+    public Message createMessage(GroupId groupId, BdfList body) throws DbException {
+        return createMessage(groupId, body, new BdfDictionary());
+    }
+
+    @Override
+    public Message createMessage(GroupId groupId, BdfList body, BdfDictionary metadata) throws DbException {
+        try {
+            byte[] bodyBytes = metadataEncoder.encode(body);
+            long timestamp = System.currentTimeMillis();
+            return messageFactory.createMessage(groupId, timestamp, bodyBytes);
+        } catch (Exception e) {
+            throw new DbException("Failed to create message", e);
+        }
+    }
+
+    @Override
+    public BdfList parseMessage(Message message) throws DbException {
+        try {
+            return metadataParser.parseList(message.getBody());
+        } catch (Exception e) {
+            throw new DbException("Failed to parse message", e);
+        }
+    }
+
+    @Override
+    public BdfDictionary parseMetadata(Message message) throws DbException {
+        try {
+            // For now, return empty metadata - this should be extracted from the message
+            return new BdfDictionary();
+        } catch (Exception e) {
+            throw new DbException("Failed to parse metadata", e);
+        }
+    }
+
+    @Override
+    public void addLocalMessage(Transaction txn, Message message, BdfDictionary metadata) throws DbException {
+        // TODO: Implement adding message to local storage
+        LOG.info("Adding local message: " + message.getId());
+    }
+
+    @Override
+    public Collection<Message> getMessages(Transaction txn, GroupId groupId) throws DbException {
+        // TODO: Implement retrieving messages from storage
+        return List.of();
+    }
+
+    @Override
+    public Collection<Message> getMessageHeaders(Transaction txn, GroupId groupId) throws DbException {
+        // TODO: Implement retrieving message headers from storage
+        return List.of();
+    }
+
+    @Override
+    public Message getMessage(Transaction txn, MessageId messageId) throws DbException {
+        // TODO: Implement retrieving specific message from storage
+        throw new DbException("Message not found: " + messageId);
+    }
+
+    @Override
+    public void deleteMessage(Transaction txn, MessageId messageId) throws DbException {
+        // TODO: Implement message deletion from storage
+        LOG.info("Deleting message: " + messageId);
+    }
+
+    @Override
+    public void deleteAllMessages(Transaction txn, GroupId groupId) throws DbException {
+        // TODO: Implement deleting all messages in group from storage
+        LOG.info("Deleting all messages in group: " + groupId);
+    }
+}
