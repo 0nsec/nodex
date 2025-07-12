edf68dd19cd72bba17e4ccb42de1eeb7bd66f2d6
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-12 21:56:42 +0000

    feat: Add MAX_GROUP_INVITATION_TEXT_LENGTH constant for private group invitations

--- nodex-api/src/main/java/org/nodex/api/privategroup/PrivateGroupConstants.java
+++ nodex-api/src/main/java/org/nodex/api/privategroup/PrivateGroupConstants.java
@@ -11,6 +11,7 @@ public class PrivateGroupConstants {
     public static final int GROUP_SALT_LENGTH = 32;
     public static final int MAX_GROUP_NAME_LENGTH = 100;
     public static final int MAX_GROUP_POST_TEXT_LENGTH = 1000;
+    public static final int MAX_GROUP_INVITATION_TEXT_LENGTH = 500;
     
     private PrivateGroupConstants() {
         // Utility class
