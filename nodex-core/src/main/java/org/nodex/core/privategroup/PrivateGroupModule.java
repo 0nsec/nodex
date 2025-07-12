461a15e5336f16416f0e0e3fc3ac59c3a9e959b9
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-12 18:01:56 +0000

    feat: Add PrivateGroupModule for Dagger dependency injection, providing private group-related components

---
+++ nodex-core/src/main/java/org/nodex/core/privategroup/PrivateGroupModule.java
@@ -0,0 +1,74 @@
+package org.nodex.core.privategroup;
+
+import org.nodex.api.privategroup.PrivateGroupFactory;
+import org.nodex.api.privategroup.PrivateGroupManager;
+import org.nodex.api.privategroup.GroupMessageFactory;
+import org.nodex.api.client.ClientHelper;
+import org.nodex.api.lifecycle.LifecycleManager;
+import org.nodex.api.sync.ValidationManager;
+import org.nodex.api.versioning.ClientVersioningManager;
+import org.nodex.api.data.MetadataEncoder;
+import org.nodex.api.data.MetadataParser;
+import org.nodex.api.system.Clock;
+import org.nodex.api.identity.IdentityManager;
+import org.nodex.api.db.DatabaseComponent;
+
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+/**
+ * Dagger module for the private group feature.
+ * Provides all private group-related dependencies.
+ */
+@Module
+public class PrivateGroupModule {
+
+    @Provides
+    @Singleton
+    PrivateGroupFactory providePrivateGroupFactory(
+            ClientHelper clientHelper,
+            Clock clock,
+            IdentityManager identityManager,
+            MetadataEncoder metadataEncoder
+    ) {
+        return new PrivateGroupFactoryImpl(clientHelper, clock, identityManager, metadataEncoder);
+    }
+
+    @Provides
+    @Singleton
+    GroupMessageFactory provideGroupMessageFactory(
+            ClientHelper clientHelper,
+            Clock clock,
+            IdentityManager identityManager,
+            MetadataEncoder metadataEncoder
+    ) {
+        return new GroupMessageFactoryImpl(clientHelper, clock, identityManager, metadataEncoder);
+    }
+
+    @Provides
+    @Singleton
+    PrivateGroupManager providePrivateGroupManager(
+            DatabaseComponent db,
+            ClientHelper clientHelper,
+            ClientVersioningManager clientVersioningManager,
+            MetadataParser metadataParser,
+            MetadataEncoder metadataEncoder,
+            Clock clock,
+            IdentityManager identityManager
+    ) {
+        return new PrivateGroupManagerImpl(db, clientHelper, clientVersioningManager, 
+                metadataParser, metadataEncoder, clock, identityManager);
+    }
+
+    @Provides
+    @Singleton
+    GroupMessageValidator provideGroupMessageValidator(
+            MetadataParser metadataParser,
+            ClientHelper clientHelper,
+            Clock clock
+    ) {
+        return new GroupMessageValidator(metadataParser, clientHelper, clock);
+    }
+}
