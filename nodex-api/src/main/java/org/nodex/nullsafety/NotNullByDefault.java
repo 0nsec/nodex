f7c820f1e6d4daefd4e8a3cd9ef5c84fb5e9b0d4
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-12 13:50:54 +0000

    Add NotNullByDefault annotation for non-null safety enforcement

---
+++ nodex-api/src/main/java/org/nodex/nullsafety/NotNullByDefault.java
@@ -0,0 +1,22 @@
+package org.nodex.nullsafety;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierDefault;
+
+/**
+ * Annotation indicating that all parameters, return values, and fields
+ * in the annotated package or class are non-null by default.
+ */
+@Documented
+@Nonnull
+@TypeQualifierDefault({ElementType.PARAMETER, ElementType.RETURN_TYPE, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PACKAGE, ElementType.TYPE})
+public @interface NotNullByDefault {
+}
