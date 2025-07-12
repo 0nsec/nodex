f293642a1eafa619c3c7dd8007aa8a427c95a0eb
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-12 16:04:03 +0000

    feat: Implement MessagingManager interface for messaging operations

---
+++ nodex-api/src/main/java/org/nodex/api/messaging/MessagingManager.java
@@ -0,0 +1,42 @@
+package org.nodex.api.messaging;
+
+import org.nodex.api.contact.ContactId;
+import org.nodex.api.db.DbException;
+import org.nodex.api.db.Transaction;
+import org.nodex.api.nullsafety.NotNullByDefault;
+import org.nodex.api.sync.GroupId;
+import org.nodex.api.sync.Message;
+
+import java.util.Collection;
+
+/**
+ * Interface for managing messaging operations
+ */
+@NotNullByDefault
+public interface MessagingManager {
+    
+    /**
+     * Sends a message to a contact
+     */
+    void sendMessage(Transaction txn, ContactId contactId, String messageText) throws DbException;
+    
+    /**
+     * Gets all messages in a conversation with a contact
+     */
+    Collection<Message> getMessages(Transaction txn, ContactId contactId) throws DbException;
+    
+    /**
+     * Gets all messages in a specific group
+     */
+    Collection<Message> getMessages(Transaction txn, GroupId groupId) throws DbException;
+    
+    /**
+     * Marks a message as read
+     */
+    void markMessageAsRead(Transaction txn, ContactId contactId, long timestamp) throws DbException;
+    
+    /**
+     * Gets the number of unread messages from a contact
+     */
+    int getUnreadMessageCount(Transaction txn, ContactId contactId) throws DbException;
+}
