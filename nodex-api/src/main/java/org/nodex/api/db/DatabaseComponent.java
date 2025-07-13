60b2d5498a32a7209f3b2ca765293403fc0eb5b4
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-13 04:40:27 +0000

    feat: Add methods for group management and message handling in DatabaseComponent interface

--- nodex-api/src/main/java/org/nodex/api/db/DatabaseComponent.java
+++ nodex-api/src/main/java/org/nodex/api/db/DatabaseComponent.java
@@ -16,4 +16,70 @@ public interface DatabaseComponent {
      * Create a new transaction.
      */
     Transaction createTransaction();
+    
+    /**
+     * Execute a transaction with result.
+     */
+    <T> T transactionWithResult(boolean readOnly, DbCallable<T, DbException> callable) throws DbException;
+    
+    /**
+     * Execute a transaction.
+     */
+    void transaction(boolean readOnly, DbRunnable<DbException> runnable) throws DbException;
+    
+    /**
+     * Get a contact by ID.
+     */
+    org.nodex.api.contact.Contact getContact(Transaction txn, org.nodex.api.contact.ContactId contactId) throws DbException;
+    
+    /**
+     * Get all contacts.
+     */
+    java.util.Collection<org.nodex.api.contact.Contact> getContacts(Transaction txn) throws DbException;
+    
+    /**
+     * Check if a group exists.
+     */
+    boolean containsGroup(Transaction txn, org.nodex.api.sync.GroupId groupId) throws DbException;
+    
+    /**
+     * Add a group.
+     */
+    void addGroup(Transaction txn, org.nodex.api.sync.Group group) throws DbException;
+    
+    /**
+     * Remove a group.
+     */
+    void removeGroup(Transaction txn, org.nodex.api.sync.Group group) throws DbException;
+    
+    /**
+     * Set group visibility.
+     */
+    void setGroupVisibility(Transaction txn, org.nodex.api.contact.ContactId contactId, 
+        org.nodex.api.sync.GroupId groupId, org.nodex.api.sync.Visibility visibility) throws DbException;
+    
+    /**
+     * Start cleanup timer for a message.
+     */
+    void startCleanupTimer(Transaction txn, org.nodex.api.sync.MessageId messageId) throws DbException;
+    
+    /**
+     * Stop cleanup timer for a message.
+     */
+    void stopCleanupTimer(Transaction txn, org.nodex.api.sync.MessageId messageId) throws DbException;
+    
+    /**
+     * Set cleanup timer duration.
+     */
+    void setCleanupTimerDuration(Transaction txn, org.nodex.api.sync.MessageId messageId, long duration) throws DbException;
+    
+    /**
+     * Set message as shared.
+     */
+    void setMessageShared(Transaction txn, org.nodex.api.sync.MessageId messageId) throws DbException;
+    
+    /**
+     * Set message as permanent.
+     */
+    void setMessagePermanent(Transaction txn, org.nodex.api.sync.MessageId messageId) throws DbException;
 }
