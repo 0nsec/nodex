c1c19563837b94082256c167363b37227d69ba71
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-13 03:30:30 +0000

    feat: Enhance BdfEntryImpl with additional type-checking methods and value retrieval functions

--- nodex-api/src/main/java/org/nodex/api/data/BdfEntryImpl.java
+++ nodex-api/src/main/java/org/nodex/api/data/BdfEntryImpl.java
@@ -22,4 +22,82 @@ public class BdfEntryImpl implements BdfEntry {
     public Object getValue() {
         return value;
     }
+    
+    @Override
+    public boolean isString() {
+        return value instanceof String;
+    }
+    
+    @Override
+    public boolean isNumber() {
+        return value instanceof Number;
+    }
+    
+    @Override
+    public boolean isBoolean() {
+        return value instanceof Boolean;
+    }
+    
+    @Override
+    public boolean isBytes() {
+        return value instanceof byte[];
+    }
+    
+    @Override
+    public boolean isList() {
+        return value instanceof BdfList;
+    }
+    
+    @Override
+    public boolean isDictionary() {
+        return value instanceof BdfDictionary;
+    }
+    
+    @Override
+    public String getString() {
+        if (value instanceof String) {
+            return (String) value;
+        }
+        throw new ClassCastException("Value is not a string");
+    }
+    
+    @Override
+    public long getLong() {
+        if (value instanceof Number) {
+            return ((Number) value).longValue();
+        }
+        throw new ClassCastException("Value is not a number");
+    }
+    
+    @Override
+    public boolean getBoolean() {
+        if (value instanceof Boolean) {
+            return (Boolean) value;
+        }
+        throw new ClassCastException("Value is not a boolean");
+    }
+    
+    @Override
+    public byte[] getBytes() {
+        if (value instanceof byte[]) {
+            return (byte[]) value;
+        }
+        throw new ClassCastException("Value is not a byte array");
+    }
+    
+    @Override
+    public BdfList getList() {
+        if (value instanceof BdfList) {
+            return (BdfList) value;
+        }
+        throw new ClassCastException("Value is not a BdfList");
+    }
+    
+    @Override
+    public BdfDictionary getDictionary() {
+        if (value instanceof BdfDictionary) {
+            return (BdfDictionary) value;
+        }
+        throw new ClassCastException("Value is not a BdfDictionary");
+    }
 }
