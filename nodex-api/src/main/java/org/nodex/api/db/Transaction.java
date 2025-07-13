0e471cba5c37bc60a1affb02cf5735f3673e4e02
Muhammad Bilal <99223413+0nsec@users.noreply.github.com>
2025-07-13 04:42:46 +0000

    feat: Add attach method to Transaction interface for event handling

--- nodex-api/src/main/java/org/nodex/api/db/Transaction.java
+++ nodex-api/src/main/java/org/nodex/api/db/Transaction.java
@@ -6,4 +6,9 @@ package org.nodex.api.db;
 public interface Transaction {
     void commit() throws DbException;
     void abort() throws DbException;
+    
+    /**
+     * Attach an event to this transaction.
+     */
+    void attach(org.nodex.api.event.Event event);
 }
